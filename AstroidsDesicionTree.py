b9d09b5dd8dbb62b927493ce627c295cf4d96da5
AndrewLinscott <alinscott1921@gmail.com>
2021-04-29 20:30:08 -0400

    refactored into functions

--- AstroidsDesicionTree.py
+++ AstroidsDesicionTree.py
@@ -4,30 +4,40 @@ from sklearn.model_selection import train_test_split    # Import train_test_spli
 from sklearn import metrics                             #Import scikit-learn metrics module for accuracy calculation
 
 astroids = pd.read_csv("./dataset/nasa.csv")
-#----------data cleaning--------------#
-astroids['Hazardous'] = astroids['Hazardous'].map({True: 1, False: 0})
 
-#----------feature selection----------#
-independent_variables=['Absolute Magnitude','Est Dia in M(min)','Est Dia in M(max)','Relative Velocity km per sec',\
-'Orbit Uncertainity','Minimum Orbit Intersection','Jupiter Tisserand Invariant','Epoch Osculation','Eccentricity',\
-'Semi Major Axis','Inclination','Asc Node Longitude','Orbital Period','Perihelion Distance','Perihelion Arg',\
-'Aphelion Dist','Perihelion Time','Mean Anomaly','Mean Motion']
+def cleandata(data:pd.core.frame.DataFrame):
+    #----------data cleaning--------------#
+    data['Hazardous'] = data['Hazardous'].map({True: 1, False: 0})
 
-dependent_variables=['Hazardous']
+    #----------feature selection----------#
+    independent_variables=['Absolute Magnitude','Est Dia in M(min)','Est Dia in M(max)','Relative Velocity km per sec',\
+    'Orbit Uncertainity','Minimum Orbit Intersection','Jupiter Tisserand Invariant','Epoch Osculation','Eccentricity',\
+    'Semi Major Axis','Inclination','Asc Node Longitude','Orbital Period','Perihelion Distance','Perihelion Arg',\
+    'Aphelion Dist','Perihelion Time','Mean Anomaly','Mean Motion']
 
-x=astroids[independent_variables]
-y=astroids[dependent_variables]
+    dependent_variables=['Hazardous']
 
+    x=data[independent_variables]
+    y=data[dependent_variables]
+    
+    return [x,y]
+
+def trainclassifier(x_train, y_train):
+    #-----------create classifer----------#
+    clf = DecisionTreeClassifier()
+    #-----------train model---------------#
+    clf = clf.fit(x_train,y_train)
+    
+    return clf
+
+
+features = cleandata(astroids)
 #-----------spliting data-------------#
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=1) # 70% training and 30% test
-
-#-----------create classifer----------#
-clf = DecisionTreeClassifier()
-
-#-----------train model---------------#
-clf = clf.fit(x_train,y_train)
+x_train, x_test, y_train, y_test = train_test_split(features[0], features[1], test_size=0.3, random_state=1) # 70% training and 30% test
+#------create trained calssifer-------#
+classifier = trainclassifier(x_train, y_train)
 #-----------test model----------------#
-y_pred = clf.predict(x_test)
+y_pred = classifier.predict(x_test)
 
 #-----------evaluate model------------#
 print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
