a0a96de57db8a757798cd74d55b9e48f9a66b96d
AndrewLinscott <alinscott1921@gmail.com>
2021-05-17 00:02:00 -0400

    graph visualization

--- AstroidsDesicionTree.py
+++ AstroidsDesicionTree.py
@@ -1,7 +1,8 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier         # Import Decision Tree Classifier
-from sklearn.model_selection import train_test_split    # Import train_test_split function
-from sklearn import metrics                             #Import scikit-learn metrics module for accuracy calculation
+import pandas as pd                                                         # Import pandas  
+from sklearn.tree import DecisionTreeClassifier, plot_tree, export_text     # Import Decision Tree Classifier, and graph tools
+from sklearn.model_selection import train_test_split                        # Import train_test_split function
+from sklearn import metrics                                                 # Import scikit-learn metrics module for accuracy calculation
+import matplotlib.pyplot as plt
 
 astroids = pd.read_csv("./dataset/nasa.csv")
 
@@ -32,7 +33,7 @@ def trainclassifier(x_train, y_train):
 
 features = cleandata(astroids)
 #-----------spliting data-------------#
-x_train, x_test, y_train, y_test = train_test_split(features[0], features[1], test_size=0.3, random_state=1) # 70% training and 30% test
+x_train, x_test, y_train, y_test = train_test_split(features[0], features[1], test_size=0.3, random_state=1) # 70% training and 30% test, 90% Ac. up to .995 test
 #------create trained calssifer-------#
 classifier = trainclassifier(x_train, y_train)
 #-----------test model----------------#
@@ -41,6 +42,10 @@ y_pred_train = classifier.predict(x_train)
 #-----------evaluate model------------#
 testAc=metrics.accuracy_score(y_test, y_pred)
 trainAc=metrics.accuracy_score(y_pred_train, y_train)
-    
-print("Accuracy for test  set:",testAc)
-print("Accuracy for train set:", trainAc)
+
+
+plt.figure(figsize=(25,20))
+print(plot_tree(classifier, feature_names=list(features[0].columns.values), filled=True))  #graphical tree display
+print(export_text(classifier, feature_names=list(features[0].columns.values)))             #test tree display
+print("Accuracy for test  set:",testAc)     #print evaluations
+print("Accuracy for train set:", trainAc)   #print evaluations
